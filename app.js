3e2507b6dce0342846283f68b6c4fb1d3a939b26
Hamza <hamzamirza857@gmail.com>
2022-03-06 10:16:35 -0800

    changing in app.js file

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const router = express.Router();
-
+const port = process.env.port || 3000;
 router.get('/',function(req,res){
   res.sendFile(path.join(__dirname+'/index.html'));
   //__dirname : It will resolve to your project folder.
@@ -18,6 +18,7 @@ router.get('/',function(req,res){
 
 //add the router
 app.use('/', router);
-app.listen(process.env.port || 3000);
-
-console.log('Running at Port 3000');
+app.listen(port, ()=>{
+    console.log('Running at Port ${port}');
+    
+});
